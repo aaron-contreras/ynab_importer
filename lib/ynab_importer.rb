e7c9039e0fe307adb1c110f15cc0aaf665c04921
Aaron Contreras <aaronlcaq@gmail.com>
2022-04-23 12:09:53 -0500

    💥 Initial commit

---
+++ lib/ynab_importer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ynab_importer/version"
+
+module YnabImporter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ynab_importer/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module YnabImporter
+  VERSION = "0.1.0"
+end
